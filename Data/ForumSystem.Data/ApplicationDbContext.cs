ea75269639fb9143c2d0b91946f6178da4f0fed2
jdimov59 <jdimov59@gmail.com>
2016-06-13 12:20:31 +0300

    Identity models refactored (extracted to libraries)

---
+++ Data/ForumSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ForumSystem.DataModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ForumSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
