76e0891f866193e2498f2221d3d22115b9e8c0ab
jdimov59 <jdimov59@gmail.com>
2016-06-13 18:56:27 +0300

    Integrating Repository Pattern
    Created interfaces for repositories

---
+++ Data/ForumSystem.Data.Common/Repository/IDeletableEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace ForumSystem.Data.Common.Repository
+{
+    public interface IDeletableEntityRepository<T>: IRepository<T> where T : class
+    {
+        IQueryable<T> AllWithDeleted();
+    }
+}
