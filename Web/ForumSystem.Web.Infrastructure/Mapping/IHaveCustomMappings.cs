dab55723011561f90f9905168958d2b346737e3c
jdimov59 <jdimov59@gmail.com>
2016-06-14 18:54:40 +0300

    AutoMapper integrated with ForumSystem.Web
    AutoMapper integrated with ForumSystem.Web
    Created library ForumSystem.Web.Infrastructure

---
+++ Web/ForumSystem.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace ForumSystem.Web.Infrastructure.Mapping
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
